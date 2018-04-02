150abbbedc3324d3785e3b9e53460917b35a5bc7
Gray Young <1162032953@qq.com>
2018-04-02 16:22:07 +0800

    微信小程序首页
    给首页添加入口
    添加我的页面
    添加系统设置页面

---
+++ 为人师表/pages/index/mine/mine.js
@@ -0,0 +1,72 @@
+// pages/index/mine/mine.js
+Page({
+
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		arraySex: ['男', '女'],
+		indexSex: 0,
+		arrayIdentity: ['学生', '老师'],
+		indexIdentity: 0,
+		date: '2018-04-01'
+	},
+	bindDateChange: function (e) {
+		console.log(e.detail.value)
+	},
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {
+
+	},
+
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {
+
+	},
+
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {
+
+	},
+
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {
+
+	}
+})
