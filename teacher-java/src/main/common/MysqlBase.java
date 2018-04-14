a96f9301b9cf010ce72f3006c68a9267061a3607
guohaozhuo <920099705@qq.com>
2018-04-14 15:59:36 +0800

    啊

--- teacher-java/src/main/common/MysqlBase.java
+++ teacher-java/src/main/common/MysqlBase.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class MysqlBase {
 	// JDBC 驱动名及数据库 URL
 	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	private static final String DB_URL = "jdbc:mysql://192.168.1.166:3306/teacher?useSSL=true";
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/teacher?useSSL=true";
 
 	// 数据库的用户名与密码，需要根据自己的设置
 	private static final String USER = "root";
--- teacher-java/src/main/controller/classes/CreateClass.java
+++ teacher-java/src/main/controller/classes/CreateClass.java
@@ -1,12 +1,22 @@
 package main.controller.classes;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import main.database.Classes;
+
 
 /**
  * Servlet implementation class CreateClass
@@ -31,12 +41,42 @@ public class CreateClass extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * 获取某堂客信息插入数据库
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		
+		response.setContentType("text/html;charset=UTF-8");
+		
+
+		
+		    Map<Integer, String> map = new HashMap<Integer, String>();
+			//获取前端数据
+			String courseid_form = request.getParameter("courseid");
+			String classroom_form = request.getParameter("classroom");
+			String classtime_form = request.getParameter("classtime");
+			String teachersay_form = request.getParameter("teachersay");
+			
+			// 转化courseid
+			int corId = Integer.parseInt(courseid_form);
+			
+			// 转化classtime
+			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+			Date date = null;
+			try {
+				date = format.parse(classtime_form);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Timestamp classTime = new Timestamp(date.getTime());
+			Classes.insert_class( corId, classTime, classroom_form, teachersay_form);
+			
+			
+
+		}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
