ec596a7d0295ad0d38607f8319c30a3edd9e8b3e
guohaozhuo <920099705@qq.com>
2018-04-17 22:25:55 +0800

    修改密码4.17

---
+++ teacher-java/src/main/controller/index/ChangPassword.java
@@ -0,0 +1,68 @@
+package main.controller.index;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSONObject;
+
+import main.common.MysqlBase;
+import main.database.Users;
+
+/**
+ * Servlet implementation class ChangPassword
+ */
+@WebServlet("/ChangPassword")
+public class ChangPassword extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ChangPassword() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String userid_form=request.getParameter("userid");
+		String oldpass_form=request.getParameter("oldpassword");
+		String newpass_form=request.getParameter("newpassword");
+		int userId =Integer.parseInt(userid_form);
+		JSONObject jsonObject=new JSONObject();
+		PrintWriter out=response.getWriter();
+		Map<String, String> map = new HashMap<String, String>();
+		map=Users.oldPassword(userId);
+		String oldpass=map.get("password");
+		
+		if(oldpass.equals(oldpass_form)) {
+			Users.update_password(userId, newpass_form);
+			jsonObject.put("STATU", "success!");
+		}else {
+			jsonObject.put("ERROR:", "wrong password!");
+			out.print(jsonObject);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
