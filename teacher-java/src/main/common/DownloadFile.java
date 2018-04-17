41ae115541527f863473e7dd0a0c41735ef05c85
Gray Young <1162032953@qq.com>
2018-04-17 08:44:49 +0800

    afasdf

--- teacher-java/src/main/common/DownloadFile.java
+++ teacher-java/src/main/common/DownloadFile.java
@@ -3,11 +3,9 @@ package main.common;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URLEncoder;
+import java.util.Base64;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import com.alibaba.fastjson.JSONArray;
 
@@ -18,22 +16,24 @@ public class DownloadFile {
 	 * @param request
 	 * @param response
 	 */
-	public static void uploadfile(HttpServletRequest request, HttpServletResponse response, String paths) {
+	public static byte[] uploadfile(HttpServletRequest request, String paths) {
 		// 将传过来的图片json字符串解析成jsonarray
 		JSONArray jsonArray = (JSONArray) JSONArray.parse(paths);
+		byte[] b = null;
 		// 循环拿到jsonarray的每个路径
 		for (int i = 0; i < jsonArray.size(); i++) {
 			String path = jsonArray.getString(i);
 			// 创建文件对象
-			String uploadPath = request.getServletContext().getRealPath(".") + File.separator + "upload";
+			String uploadPath = request.getServletContext().getRealPath(".") + File.separator + "upload" + File.separator ;
 			File f = new File(uploadPath + path);
 			if (f.exists()) {
 				FileInputStream fis;
 				try {
 					fis = new FileInputStream(f);
-					String filename = URLEncoder.encode(f.getName(), "utf-8"); // 解决中文文件名下载后乱码的问题
-					byte[] b = new byte[fis.available()];
+					b = new byte[fis.available()];
 					fis.read(b);
+					/*
+				 	String filename = URLEncoder.encode(f.getName(), "utf-8"); // 解决中文文件名下载后乱码的问题
 					response.setCharacterEncoding("utf-8");
 					response.setHeader("Content-Disposition", "attachment; filename=" + filename + "");
 					// 获取响应报文输出流对象
@@ -43,13 +43,14 @@ public class DownloadFile {
 
 					out.flush();
 					out.close();
+					*/
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		}
-
+		return b;
 	}
 
 }
